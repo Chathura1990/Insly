b69a266b5743269b10e06ccadab3e3eeff7796b7
Chathura <crajapakse@itsupportme.by>
2019-08-15 10:56:33 +0300

    removed WARN

--- src/main/java/testFiles/signupPageTests/SignupPageTest.java
+++ src/main/java/testFiles/signupPageTests/SignupPageTest.java
@@ -1,6 +1,5 @@
 package testFiles.signupPageTests;
 
-import applicationFiles.app_manager.model_data.SignupPageData;
 import applicationFiles.app_manager.test_base.TestBase;
 import org.testng.annotations.Test;
 
@@ -8,8 +7,6 @@ import static applicationFiles.app_manager.ApplicationManager.log;
 import static applicationFiles.app_manager.ApplicationManager.reportLog;
 
 public class SignupPageTest extends TestBase {
-
-    private SignupPageData spd = new SignupPageData();
 
     @Test(priority = 1)
     public void completeSignupPage() throws InterruptedException {
